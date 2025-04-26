3702679bc9c539235fa7d81ff24d62646949f948
hieuc <hieuchung428@gmail.com>
2025-04-26 10:29:29 +0700

    update migrations

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/congiong', function () {
+    return view('congiong');
+})->name('congiong');
+
+
