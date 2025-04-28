76903f6e055db50efa35bb6ba22050ce55bf3ea6
hieuc <hieuchung428@gmail.com>
2025-04-28 23:15:59 +0700

    sua confict web

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('page.congiong', function () {
+    return view('congiong');
+})->name('congiong');
+
