31d8a2f0fafc4446018a35a58ff38dc2e32d5e2f
hieuc <hieuchung428@gmail.com>
2025-05-05 20:40:06 +0700

    add cart

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;use App\Http\Controllers\{ HomeController };
+use Illuminate\Support\Facades\Route;use App\Http\Controllers\{ HomeController,  CartController };
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +17,10 @@ Route::get('/congiong', [HomeController:: class, 'congiong']);
 Route::get('/search', [HomeController:: class, 'search'])->name('search');
 Route::get('/viewAll', [HomeController:: class, 'viewAll'])->name('viewAll');
 Route::get('/services', [HomeController:: class, 'services'])->name('services');
+  // router cart
+  Route::get('cart', [CartController::class, 'cart'])->name('cart');
+Route::get('add-to-cart/{id}', [CartController::class, 'addToCart'])->name('add_to_cart');
+Route::get('add-go-to-cart/{id}', [CartController::class, 'addGoToCart'])->name('add_go_to_cart');
+Route::patch('update-cart', [CartController::class, 'update'])->name('update_cart');
+Route::delete('remove-from-cart', [CartController::class, 'remove'])->name('remove_from_cart');
 
