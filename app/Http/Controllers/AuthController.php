41805b97793673430e639901fda001520ea8e530
hieuc <hieuchung428@gmail.com>
2025-05-09 15:09:48 +0700

    kt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,42 +27,25 @@ class AuthController extends Controller
         return back()->with('thongbao', 'Đăng ký tài khoản thành công');
     }
 
-    // public function loginPost(Request $request){
-    //     $credetials = [
-    //         'email' => $request->email,
-    //         'password' => $request->password
-    //     ];
-    //     if (Auth::guard('khachhang')->attempt($credetials)) {
-    //         return redirect('/')->with('thongbao', 'Đăng nhập thành công');
-    //     }
+    public function loginPost(Request $request){
+        $credetials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        if (Auth::guard('khachhang')->attempt($credetials)) {
+            return redirect('/')->with('thongbao', 'Đăng nhập thành công');
+        }
     
 
-    //     if(Auth::attempt($credetials)){
-    //         return redirect('/')->with('thongbao', 'Đăng nhập thành công');
-    //     }
-
-    //     return back()->with('error', 'Sai tên tài khoản hoặc mật khẩu');
-    // }
-public function loginPost(Request $request)
-{
-    $credentials = $request->only('email', 'password');
-
-    if (Auth::attempt($credentials)) {
-        $request->session()->regenerate();
-        $user = Auth::user();
-
-        if ($request->type === 'admin' && $user->id_phanquyen == 1) {
-            return redirect()->route('admin.dashboard');
-        } elseif ($request->type === 'khachhang' && $user->id_phanquyen == 2) {
-            return redirect()->route('home');
-        } else {
-            Auth::logout();
-            return back()->with('error', 'Bạn không có quyền truy cập');
+        if(Auth::attempt($credetials)){
+            return redirect('/')->with('thongbao', 'Đăng nhập thành công');
         }
+
+        return back()->with('error', 'Sai tên tài khoản hoặc mật khẩu');
     }
 
-    return back()->with('error', 'Sai email hoặc mật khẩu!');
-}
+
+
 
     public function logout(){
         Auth::logout();
